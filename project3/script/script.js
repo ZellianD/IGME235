99f8eea74bcf00d4dd79236352652a7c3e1b9ec2
Zell <ZellianDettmer@gmail.com>
2024-12-13 13:21:24 -0500

    fuck me nothing works anymore

--- project3/script/script.js
+++ project3/script/script.js
@@ -5,47 +5,102 @@
     const MAX_IMAGE_LEFT = 770;
     const LINE_HEIGHT = 30; 
     const IMAGE_MARGIN = 80;
-    const IMAGE_SPACING = 80;   
+    const IMAGE_SPACING = 0;   
     const MAX_Z_INDEX = 1000; 
 
-    let squish;
+    let sound;
+    const HAND = [
+        `img/bone_background.png`,
+        `<a target="_blank" href="img/hand_full.png">Hint</a>`,
+        './sounds/bone.mp3',
+        `<img class ="pic" src = "img/hand_1.png" alt="hand 1">`,
+        `<img class ="pic" src = "img/hand_2.png" alt="hand 2">`,
+        `<img class ="pic" src = "img/hand_3.png" alt="hand 3">`,
+        `<img class ="pic" src = "img/hand_4.png" alt="hand 4">`,
+        `<img class ="pic" src = "img/hand_5.png" alt="hand 5">`,
+        `<img class ="pic" src = "img/hand_6.png" alt="hand 6">`,
+        `<img class ="pic" src = "img/hand_7.png" alt="hand 7">`,
+        `<img class ="pic" src = "img/middle_bone_1.png" alt="middle bone 1">`,
+        `<img class ="pic" src = "img/middle_bone_2.png" alt="middle bone 2">`,
+        `<img class ="pic" src = "img/middle_bone_3.png" alt="middle bone 3">`,
+        `<img class ="pic" src = "img/middle_bone_4.png" alt="middle bone 4">`,
+        `<img class ="pic" src = "img/pinky_bone_1.png" alt="pinky bone 1">`,
+        `<img class ="pic" src = "img/pinky_bone_2.png" alt="pinky bone 2">`,
+        `<img class ="pic" src = "img/pinky_bone_3.png" alt="pinky bone 3">`,
+        `<img class ="pic" src = "img/pinky_bone_4.png" alt="pinky bone 4">`,
+        `<img class ="pic" src = "img/pointer_bone_1.png" alt="pointer bone 1">`,
+        `<img class ="pic" src = "img/pointer_bone_2.png" alt="pointer bone 2">`,
+        `<img class ="pic" src = "img/pointer_bone_3.png" alt="pointer bone 3">`,
+        `<img class ="pic" src = "img/pointer_bone_4.png" alt="pointer bone 4">`,
+        `<img class ="pic" src = "img/radius_bone.png" alt="radius">`,
+        `<img class ="pic" src = "img/ring_bone_1.png" alt="ring bone 1">`,
+        `<img class ="pic" src = "img/ring_bone_2.png" alt="ring bone 2">`,
+        `<img class ="pic" src = "img/ring_bone_3.png" alt="ring bone 3">`,
+        `<img class ="pic" src = "img/ring_bone_4.png" alt="ring bone 4">`,
+        `<img class ="pic" src = "img/thumb_bone_1.png" alt="thumb bone 1">`,
+        `<img class ="pic" src = "img/thumb_bone_2.png" alt="thumb bone 2">`,
+        `<img class ="pic" src = "img/thumb_bone_3.png" alt="thumb bone 3">`,
+        `<img class ="pic" src = "img/ulna_bone.png" alt="ulna">`
+    ];
+    const BEETLE =[
+        `img/beetle_background.png`,
+        `<a target="_blank" href="img/beetle_full.png">Hint</a>`,
+        './sounds/beetle.mp3',
+        `<img class ="pic" src = "img/top_arm_left.png" alt="1">`,
+        `<img class ="pic" src = "img/top_arm_right.png" alt="2">`,
+        `<img class ="pic" src = "img/body.png" alt="3">`,
+        `<img class ="pic" src = "img/head.png" alt="4">`,
+        `<img class ="pic" src = "img/bottom_arm_left.png" alt="5">`,
+        `<img class ="pic" src = "img/bottom_arm_right.png" alt="6">`,
+        `<img class ="pic" src = "img/middle_arm_left.png" alt="7">`,
+        `<img class ="pic" src = "img/middle_arm_right.png" alt="8">`,
+        `<img class ="pic" src = "img/wing_left.png" alt="9">`,
+        `<img class ="pic" src = "img/wing_right.png" alt="11">`,
+        `<img class ="pic" src = "img/wing_cover_left.png" alt="22">`,
+        `<img class ="pic" src = "img/wing_cover_right.png" alt="33">`
+    ];
 
-    const IMAGES = [`<img class="pic" src= "img/Eye.png" alt="">`,`<img class="pic" src= "img/Eye1.png" alt="">`,`<img class="pic" src= "img/Eye2.png" alt="">`]
-    //create consts to create a boarder that they cannot cross
+ 	window.onload = (e) => {randomPuzzle(); positionIMGS(); setupDragging();};
 
- 	window.onload = (e) => {setupIMGS(); positionIMGS(); setupDragging();};
-
-    //add to a class
-    function setupIMGS()
+    function randomPuzzle()
     {
-        for(let img of IMAGES)
-        {
-            document.querySelector("#tray").innerHTML += img;
+        let ranNum = Math.random();
+        if(ranNum > .5){
+            document.querySelector("#tray").style.height = "275px";
+            document.querySelector("footer").style.marginTop = "805px";
+            setupIMGS(HAND);
         }
+        else{
+            document.querySelector("#tray").style.height = "600px";
+            document.querySelector("#tray").style.width = "850px";
+            document.querySelector("footer").style.marginTop = "1130px";
+            setupIMGS(BEETLE);
+        }
+    }
 
-        squish = new Audio('./sounds/squish.mp3');
-        // let testing = document.querySelector("#tray");
-        // testing.innerHTML=`<img class="pic" src= "img/Eye.png" alt="">`;
+    function setupIMGS(puzzle)
+    {
+        for(let i = 3; i<puzzle.length; i++)
+        {
+            document.querySelector("#tray").innerHTML += puzzle[i];
+        }
+        document.querySelector("#board").style.backgroundImage=`url(../${puzzle[0]})`;
+        document.querySelector("#sidetray").innerHTML += `${puzzle[1]}`;
+
+        sound = new Audio(`${puzzle[2]}`);
     }
     function setPosition(word,wordLeft,wordTop)
     {
-        //changes the css to the declared position for the word
         word.style.left = wordLeft + "px";
         word.style.top = wordTop + "px";
-        word.style.transform = `rotate(${(Math.random() - 0.5)* 30}deg)`;
+        word.style.transform = `rotate(${(Math.random() - 0.5)* 10}deg)`;
     }
     function positionIMGS()
     {
         let doMouseDown = (e) => {
             e.preventDefault();
-            squish.play();
+            sound.play();
             selectedElement = e.target;
-            //create something that checks and makes sure everything is within the bounds...which
-            //ive think theyve done weird here because i dont think there IS bounds
-            // if(selectedElement.x > TRAY_POSITION.x||selectedElement.y > TRAY_POSITION.y)
-            // {
-            //     setPosition(selectedElement,());
-            // }
             selectedElement.style.zIndex = MAX_Z_INDEX; 
         };
         let allIMGS = document.querySelectorAll(".pic");
@@ -56,13 +111,13 @@
         for(let image of allIMGS)
         {
             setPosition(image,imgLeft,imgTop)
-            imgLeft += imgSpacing;
+            imgLeft += imgSpacing+image.naturalWidth;
             
-            // if(wordLeft>= MAX_IMAGE_LEFT)
-            // {
-            //     wordLeft = IMAGE_MARGIN;
-            //     wordTop += LINE_HEIGHT;
-            // }
+            if(imgLeft>= MAX_IMAGE_LEFT)
+            {
+                imgLeft = IMAGE_MARGIN;
+                imgTop += LINE_HEIGHT + image.naturalHeight;
+            }
             image.onmousedown = doMouseDown;
         }
         //allows for movement of the title bars
